088f77d9d049a8cca95301b4ab32083eccbc7ca1
Miika Pynttäri <miika.pynttari@gmail2.com>
2021-12-16 15:25:17 +0200

    ...

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -25,6 +26,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(cors({origin: "http://localhost:3000", optionsSuccessStatus: 200}))
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
